a5329ddade9ea7923ad4d09a9e4dfc2582bfb611
matgbar <mbarstead@gmail.com>
2020-01-18 22:05:47 -0500

    Significant progress on the imputation refactor, still needs re-org and some more utilities

--- R/general_utils.R
+++ R/general_utils.R
@@ -67,3 +67,133 @@ ini_global_reactive_values <- function(){
 range01 <- function(x){
   (x-min(x, na.rm = T))/(max(x, na.rm = T)-min(x, na.rm = T))
 }
+
+
+#' Internal utility for estimating mode of posterior distribution from imputation models
+#'
+#' \code{estimate_mode} takes the posterior samples from a \code{stanfit} object and returns the mode of the posterior
+#' distribution
+#'
+#' @param x a \code{numeric} series, vector, or matrix of values
+#'
+#' @return maximum a posteriori value of the input object
+#'
+#' @export
+
+estimate_mode <- function(x){
+  d <- density(x)
+  return(d$x[which.max(d$y)])
+}
+
+
+#' Internal utility for maximizing imputation data range
+#'
+#' \code{generate_imputation_input_windows} is designed to take in the time vector for the entire PPG series, and return
+#' a list containing the boundaries that define input data that will be used in the imputation model. The total amount
+#' of input data is defined by the \code{expansion_factor}, which is the number of total respiration cycles. The value
+#' in \code{ibiVizEdit} defaults to 3, and in the case of imputation models. Combined with the average respiration rate
+#' extracted from the PPG and IBI series, the \code{total_input_time} parameter represents the ideal of being able to
+#' use approximately three breaths worth of data before the window targeted for imputation and three breaths worth of
+#' data after the target window. When the target window approaches the beginning or end of a file, this function will
+#' distribute the total time around the target window as evenly as possible. The goal is to come as close as possible to
+#' the total amount of input data covering the amount of time defined by \code{total_input_time}.
+#'
+#' @param time_vector is a \code{numeric} vector that measures the passage of time for the entire PPG signal
+#' @param total_input_time is a \code{numeric} 1D vector based on the average number of seconds between breaths
+#' (derived) from a statistical analysis of the PPG and IBI signals, adjusting for user-defined age (see
+#' \code{estimate_average_respiration()}). This respiration interval length is then multiplied by an "expansion" factor.
+#' The default value for this expansion factor in \code{ibiVizEdit} is 3.
+#' @param target_time_min is a \code{numeric} 1D vector that contains the minimum time value of the user-defined
+#' imputation window.
+#' @param target_time_max is a \code{numeric} 1D vector that contains the maximum time value of the user-defined
+#' imputation window.
+#'
+#' @export
+#'
+
+generate_imputation_input_windows <- function(time_vector, total_input_time, target_time_min, target_time_max){
+  time_vector_min <- min(time_vector, na.rm = TRUE)
+  time_vector_max <- max(time_vector, na.rm = TRUE)
+  pre_bounds <- c(target_time_min - total_input_time/2, target_time_min)
+  post_bounds <- c(target_time_max, target_time_max + total_input_time/2)
+
+  # Logic that attempts to balance and maxiimze the amount of input data around the target window.
+  if(pre_bounds[1] < time_vector_min | post_bounds[2] > time_vector_max){
+    if(pre_bounds[1] < time_vector_min & post_bounds[2] > time_vector_max){
+      pre_bounds[1] <- time_vector_min
+      post_bounds[2] <- time_vector_max
+    }
+    else if(pre_bounds[1] < time_vector_min & post_bounds[2] < time_vector_max){
+      time_diff <- time_vector_min - pre_bounds[1]
+      pre_bounds[1] <- time_vector_min
+      max_opts <- c(post_bounds[2] + time_diff, time_vector_max)
+      post_bounds[2] <- max_opts[which.min(max_opts)]
+    }
+    else if(pre_bounds[1] > time_vector_min & post_bounds[2] > time_vector_max){
+      time_diff <- post_bounds[2] - time_vector_max
+      post_bounds[2] <- time_vector_max
+      min_opts <- c(pre_bounds[1] - time_diff, time_vector_min)
+      pre_bounds[1] <- min_opts[which.max(min_opts)]
+    }
+  }
+
+  actual_input_time <- (pre_bounds[2] - pre_bounds[1]) + (post_bounds[2] - post_bounds[1])
+
+  # Generate a simple warning message in the console if the actual differs by 3 seconds or more from desired amount
+  if(actual_input_time < total_input_time-3){
+    warning("Total input data for imputation model is below the recommended total, based on average respiration.", "\n",
+            "Recommended total amount of input data in seconds (pre + post): ", round(total_input_time, 2), "\n",
+            "Actual amount of input data in seconds: ", round(actual_input_time, 2))
+  }
+
+  # Generate warning messages in the console if the actual amount of data is low, and control whether actual values are
+  # returned. The NULL value assignments will be used to contol modal dialog box displays when there is insufficient
+  # data for the imputation model.
+  if(actual_input_time < 5){
+    warning("Input data for imputation model is below an acceptable total. The model cannot generate stable", "\n",
+            "estimates with less than 5 seconds of input data. It may be that your file is too small to consider an", "\n",
+            "imputation approach")
+    return(list(pre=NULL, post=NULL))
+  }
+  else if(actual_input_time<10){
+    warning("Input data for imputation model is relatively low. The model may not be able to generate stable", "\n",
+            "estimates with less than 10 seconds of input data. It may be that your file is too small to consider an", "\n",
+            "imputation approach. Inspect results carefully.")
+    return(list(pre=pre_bounds, post=post_bounds))
+  }
+  else{
+    return(list(pre=pre_bounds, post=post_bounds))
+  }
+}
+
+
+#' Internal utility for deteming average respiration jointly using PPG and IBI signals
+#'
+
+estimate_avg_respiration <- function(ibi_data=NULL, ibi_col="IBI", time_col="Time", respiration_cat=NULL, ds = NULL,
+                                     respiration_mapping=AVERAGE_RESPIRATION_BY_AGE){
+  respiration_bounds <- respiration_mapping[respiration_cat][[1]]/60/ds
+  time_df <- data.frame(time_col=seq(min(ibi_data[time_col], na.rm = TRUE),
+                                     max(ibi_data[time_col], na.rm = TRUE), by = .01))
+  ibi_data <- merge(time_df, ibi_data, by=time_col, all=TRUE)
+  ibi_data[ibi_col] <- na_kalman(ibi_data[time_col])
+
+  ibi_filtered <- bwfilter(ts(ibi_data[ibi_col], frequency = 100), from = respiration_bounds[1]*ds,
+                           to = respiration_bounds[2]*ds, bandpass = TRUE, f = 100)
+
+  spec_ibi <- mvspec(ibi_filtered, spans=c(7, 7), taper=.1, demean=TRUE, log='no', plot=FALSE)
+
+  # Truncating data to expected respiration frequency range.
+  # The more consistent and cleaner the signal across ppg and ibi waverforms, the stronger prior for the average
+  spec_ibi <- data.frame(freq = spec_ibi$freq[spec_ibi$freq >= respiration_bounds[1] &
+                                                spec_ibi$freq <= respiration_bounds[2]],
+                         dens = spec_ibi$spec[spec_ibi$freq >= respiration_bounds[1] &
+                                                spec_ibi$freq <= respiration_bounds[2]])
+
+  # Taking a weighted average using spectral density weights from each signal as weights of respiration frequency
+  # Multiplying back out by ds (the downsampling rate) to return frequency to Hz
+  mean_resp <- (spec_ibi$freq*spec$dens)/sum(spec_ibi$dens)*ds
+
+  return(mean_resp)
+}
+
--- R/gp_impute_Bayes.R
+++ R/gp_impute_Bayes.R
@@ -1,4 +1,3 @@
-#gp_impute_Bayes
 AVERAGE_RESPIRATION_BY_AGE <- list(`Young Infant (<1 yr)` = c(30, 60),
                                    `Infant/Toddler (1 to 3 yrs)` =c (24, 40),
                                    `Young Child (3 to 6 yrs)` =c (22, 34),
@@ -6,9 +5,12 @@ AVERAGE_RESPIRATION_BY_AGE <- list(`Young Infant (<1 yr)` = c(30, 60),
                                    `Adolescent (12 to 18 yrs)` = c(12, 16),
                                    `Adult (18+ yrs)`= c(12, 20))
 
+#' Internal utility that aggregates data and settings needed to trigger an imputation model run
+#'
+
 gp_impute_driver <- function(iter=NULL, warmup=NULL, adapt_delta=NULL, ibi_min=NULL, ibi_max=NULL, time_min=NULL,
                              time_max=NULL, ppg_data=NULL, ppg_col="PPG", ibi_data=NULL, ibi_col="IBI", time_col="Time",
-                             expansion_factor=3, respiration_cat=NULL){
+                             expansion_factor=3, respiration_cat=NULL, ds=NULL){
   driver <- list()
   if(iter<=warmup){
     warning(paste("The number of total iterations must exceed the number of warmup iterations.", "\n",
@@ -22,37 +24,88 @@ gp_impute_driver <- function(iter=NULL, warmup=NULL, adapt_delta=NULL, ibi_min=N
   driver$ibi_max <- ibi_max
   driver$prediction_window <- c(time_min, time_max)
   driver$mean_respiration <- estimate_avg_respiration(ppg_data, ppg_col, ibi_data, ibi_col, respiration_cat,
-                                                      AVERAGE_RESPIRATION_BY_AGE)
+                                                      AVERAGE_RESPIRATION_BY_AGE, ds)
+
 }
 
 
-estimate_avg_respiration <- function(ppg_data=NULL, ppg_col=NULL, ibi_data=NULL, ibi_col="IBI", respiration_cat=NULL,
-                                     respiration_mapping=AVERAGE_RESPIRATION_BY_AGE, ds=NULL){
-  respiration_bounds <- respiration_mapping[respiration_cat][[1]]/60/ds
-  spec_ppg <- mvspec(ppg_data[ppg_col], spans=c(7, 7), taper=.1, demean=TRUE, log='no', plot=FALSE)
-  spec_ibi <- mvspec(ibi_data[ibi_col], spans=c(7, 7), taper=.1, demean=TRUE, log='no', plot=FALSE)
+#' Internal utility for generating imputation input data
+#'
+#' \code{generate_model_ppg_inputs} is used to generate a \code{data.frame} that contains an appropriately down-sampled
+#' set of "Time" and "PPG" inputs for the imputation model. These inputs represent PPG data that "surrounds" the window
+#' selected for imputation. The size of the window is dynamically determined for each participant based on average
+#' respiration rate (derived from \code{estimate_avg_respiration()}).
+#'
+#' @param time_min is a \code{numeric} 1D vector that contains the minimum time value of the user-defined imputation
+#' window.
+#' @param time_max is a \code{numeric} 1D vector that contains the maximum time value of the user-definedimputation
+#' window.
+#' @param ppg_data is a \code{data.frame} that contains the processed PPG signal and a time variable.
+#' @param ppg_col is of type \code{character} and is the column name in the \code{ppg_data} that contains the PPG signal
+#' @param time_col is of type \code{character} and is the column name in the \code{ppg_data} that contains the time
+#' variable
+#' @param expansion_factor is a \code{integer} that represents the number of average respiration cycles the program will
+#' use when exapanding to select input data for the imputation model.
+#' @param respiration_cycle_time is a \code{numeric} value that indicates the average number of seconds elapsed during
+#' each respiration cycle. The value is derived from \code{estimate_avg_respiration()}
+#' @param ds is a \code{integer} that represents the final "downsampled" rate of the PPG data, in Hz
+#'
+#' @export
+#'
 
-  # Truncating data to expected respiration frequency range.
-  # The more consistent and cleaner the signal across ppg and ibi waverforms, the stronger prior for the average
-  spec_ppg <- data.frame(freq = spec_ppg$freq[spec_ppg$freq >= respiration_bounds[1] &
-                                                spec_ppg$freq <= respiration_bounds[2]],
-                         dens = spec_ppg$spec[spec_ppg$freq >= respiration_bounds[1] &
-                                                spec_ppg$freq <= respiration_bounds[2]])
+generate_model_ppg_inputs <- function(time_min=NULL, time_max=NULL, ppg_data=NULL, ppg_col="PPG", time_col="Time",
+                                      expansion_factor=NULL, respiration_cycle_time=NULL, ds=NULL){
+  total_time <- 2*expansion_factor*respiration_cycle_time
+  input_bounds_list <- generate_imputation_input_windows(ppg_data[time_col], total_time, timen_min, time_max)
+  sample_rate <- round(ds/12)
 
-  spec_ibi <- data.frame(freq = spec_ibi$freq[spec_ibi$freq >= respiration_bounds[1] &
-                                                spec_ibi$freq <= respiration_bounds[2]],
-                         dens = spec_ibi$spec[spec_ibi$freq >= respiration_bounds[1] &
-                                                spec_ibi$freq <= respiration_bounds[2]])
+  # Creating a basic set of guardails here to propagate forward presence/effects of NULL values
+  if(!is.null(input_bounds_list$pre) & !is.null(input_bounds_list$post)){
+    time_pre <- ppg_data[time_col][between(ppg_data[time_col], input_bounds_list$pre[1], input_bounds_list$pre[2])]
+    time_post <- ppg_data[time_col][between(ppg_data[time_col], input_bounds_list$post[1], input_bounds_list$post[2])]
+    ppg_pre <- ppg_data[ppg_col][between(ppg_data[time_col], input_bounds_list$pre[1], input_bounds_list$pre[2])]
+    ppg_post <- ppg_data[ppg_col][between(ppg_data[time_col], input_bounds_list$post[1], input_bounds_list$post[2])]
+  }
 
-  # Taking a weighted average using spectral density weights from each signal as weights of respiration frequency
-  # Multiplying back out by ds (the downsampling rate) to return frequency to Hz
-  mean_resp <- (spec_ppg$freq*spec_ppg$dens + spec_ibi$freq*spec$dens)/(spec_ppg$dens + spec_ibi$dens)*ds
+  # Enforcing guardrails for processing steps that could return NULLs as invalid processing outputs
+  if(exists("time_pre") & exists("ppg_pre")){
+    if(length(time_pre) == length(ppg_pre)){
+      select_seq <- seq(1, length(time_pre), by=sample_rate)
+      time_pre <- time_pre[select_seq]
+      ppg_pre <- ppg_pre[select_seq]
+    }
+    else{
+      warning("Input time vector and ppg vector do not match in length. Specify your imputation window again.", "\n",
+              "If this continues to happen either contact the developer (mbarstead@deadreckoning.consulting), or", "\n",
+              "submit an issue on GitHub: https://github.com/matgbar/IBI_VizEdit")
+      time_pre <- NULL
+      ppg_pre <- NULL
+    }
+  }
 
-  return(mean_resp)
-}
+  # Enforcing guardrails for processing steps that could return NULLs as invalid processing outputs
+  if(exists("time_post") & exists("ppg_post")){
+    if(length(time_post) == length(ppg_post)){
+      select_seq <- seq(1, length(time_post), by=sample_rate)
+      time_post <- time_post[select_seq]
+      ppg_post <- ppg_post[select_seq]
+    }
+    else{
+      warning("Input time vector and ppg vector do not match in length. Specify your imputation window again.", "\n",
+              "If this continues to happen either contact the developer (mbarstead@deadreckoning.consulting), or", "\n",
+              "submit an issue on GitHub: https://github.com/matgbar/IBI_VizEdit")
+      time_post <- NULL
+      ppg_post <- NULL
+    }
+  }
 
+  # Return an empty object if there is an error in this processing step - will be govern display of user warnings
+  if(is.null(time_pre) | is.null(time_post)){
+    inputs_df <- NULL
+  }
+  else{
+    inputs_df <- data.frame(Time=c(time_pre, time_post), PPG=c(ppg_pre, ppg_post))
+  }
 
-model_ppg_inputs <- function(time_min=NULL, time_max=NULL, ppg_data=NULL, ppg_col="PPG", ppg_time_col="Time",
-                             expansion_factor=NULL){
-
+  return(inputs_df)
 }
