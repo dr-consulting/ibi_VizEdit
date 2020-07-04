39b5f922717f1f829520c7176965d0227287cc0d
dr-consulting <mbarstead@deadreckoning.com>
2020-07-04 12:29:55 -0400

    add test stub file for find_ibis

---
+++ tests/testthat/test-find_ibis.R
@@ -0,0 +1,17 @@
+source(system.file("utils", "find_ibis.R", package = "ibiVizEdit"))
+
+test_that("find_ibis: integration test - using static data", {
+  # test stub
+})
+
+test_that("find_peaks: unit-testing with mathematically determined peaks", {
+  # test stub
+})
+
+test_that("sum_rev: unit-testing with deterministic behavior", {
+  # test stub
+})
+
+test_that("time_sum: unit-testing with deterministic behavior", {
+  # test stub
+})
