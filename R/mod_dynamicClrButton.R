afd1f7262f0ceed2b3f206b0b19e5baed65727d5
dr-consulting <mbarstead@deadreckoning.consulting>
2020-07-12 13:00:33 -0400

    add docstrings to dynamic color button module

--- R/mod_dynamicClrButton.R
+++ R/mod_dynamicClrButton.R
@@ -1,5 +1,7 @@
 #' UI module for \code{ibiVizEdit} that serves as a basic wrapper and enables single actionButton generation.
-#'
+#' 
+#' @param id value for the namespace that links the server and ui components of the module
+#' @param inline setting passed on to the ui defining whether the button should be placed in line or not
 #' @importFrom shiny uiOutput
 #' @noRd
 
@@ -11,6 +13,16 @@ dynamicClrButtonModUI <- function(id=NULL, inline=FALSE){
 
 #' Server module for \code{ibiVizEdit} that dynamically switches actionButton UIs based on color
 #'
+#' @param input,output,session internal {shiny} parameters
+#' @param status_name a value passed via internal processes of {ibiVizEdit} determining whether a given button is 
+#' available for use.
+#' @param label the initial label presented on the button rendered in the ui 
+#' @param updated_label the updated if there is a change in label that corresponds with a change in status
+#' @param default_display_name default name displayed in the {ibiVizEdit} ui
+#' @param button_name default set to "click_in" used to define the connection between the server and ui modules
+#' @param active_color the active color - defaults to settings defined in {ibiVizEdit} internals
+#' @param inactive_color the inactive color - defaults to settings defined in {ibiVizEdit} internals
+#' @param updated_color the updated color (when active) - defaults to settings defined in {ibiVizEdit} internals
 #' @importFrom shiny actionButton
 #' @noRd
 
